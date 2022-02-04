67b5dce208dc14b7e90e742eb3947460d48a7073
SlavaKosarev <slava.kosarev55@gmail.com>
2022-02-04 20:07:32 +0300

    Revert "changed components & created messageList"

--- src/components/Message/index.js
+++ src/components/Message/index.js
@@ -1,5 +1,13 @@
-export { Message } from "./Message";
+import React from "react";
+import styles from "./Message.module.css" // подключение модулем
 
-
-
-
+export const Message = (props) => {
+   return (
+      // подключение нескольких классов
+      <div className={
+         `${styles.textColor} ${styles.textFont}`
+      }>
+         {props.children}
+      </div >
+   )
+}
