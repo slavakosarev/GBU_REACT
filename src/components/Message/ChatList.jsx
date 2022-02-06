729e5360772879db6f2c58f74672f655ddca3005
slavakosarev <slava.kosarev55@gmail.com>
2022-02-07 01:16:04 +0300

    upload

--- src/components/Message/ChatList.jsx
+++ src/components/Message/ChatList.jsx
@@ -2,11 +2,14 @@ import { List } from '@mui/material';
 import propTypes from 'prop-types';
 import { Chat } from './Chat';
 
-export const ChatList = ({ list }) => {
+export const ChatList = ({ list, onDelete }) => {
    return (
       <List>
          {list.map((item) => (
-            <Chat key={item.id} {...item} />
+            <Chat
+               onClick={() => onDelete(item.id)}
+               key={item.id} {...item}
+            />
          ))}
          <Chat id="1" name="nochat" />
       </List>
--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,6 @@
 import { createStore } from 'redux';
 import { profileReducer } from './profile/reducer';
-import { profileReducer } from './messages/reducer';
+
 
 export const store = createStore(profileReducer,
    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
