cb00b967b4e2d8943f0aac01325d1c26fd357dd1
slavakosarev <slava.kosarev55@gmail.com>
2022-02-08 19:19:01 +0300

    connected react-thunk

--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,11 @@
 import { createStore, combineReducers } from 'redux';
+import { thunk, compose, applyMiddleware } from 'redux-thunk';
 
 import { chatsReducer } from "./chats/reducer";
 import { profileReducer } from './profile/reducer';
 import { messagesReducer } from "./messages/reducer";
 
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 export const store = createStore(
    combineReducers({
@@ -11,5 +13,5 @@ export const store = createStore(
       profile: profileReducer,
       messages: messagesReducer,
    }),
-   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+   composeEnhancers(applyMiddleware(thunk))
 );
